af2e9395e467c37b85e10561c2a2e59046c9fb7d
danielchk <hollowdaniel1@gmail.com>
2020-02-05 18:53:20 +0000

    fixing 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
-/*Comment*/
+/*Comment
+*
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
