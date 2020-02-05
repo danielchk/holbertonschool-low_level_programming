ce229e7eb6f1466144e727359a23df12e5e887c2
danielchk <hollowdaniel1@gmail.com>
2020-02-05 20:05:25 +0000

    fixing 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,9 @@
 #include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 printf("Size of a char: %lu.\n", sizeof(char));
@@ -7,4 +12,4 @@ printf("Size of long int: %lu.\n", sizeof(long int));
 printf("Size of long long int: %lu.\n", sizeof(long long int));
 printf("Size of float: %lu.\n", sizeof(float));
 return (0);
-} 
+}
