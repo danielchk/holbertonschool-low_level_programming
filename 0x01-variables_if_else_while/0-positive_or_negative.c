59f9b3b3aeecde69bd7c1c888267037eaabbafaa
danielchk <hollowdaniel1@gmail.com>
2020-02-06 18:24:32 +0000

    fixing 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,9 +15,9 @@ int main(void)
 
 	if (n > 0)
 	{printf("%d is positive\n", n); }
-	else if (n == 0)
+	if (n == 0)
 	{printf("%d is zero\n", n); }
-	else if
+	if
 	{printf("%d is negative\n", n); }
 
 	return (0);
