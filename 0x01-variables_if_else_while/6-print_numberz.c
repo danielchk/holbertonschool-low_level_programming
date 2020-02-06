174ff096f6c449ae3be8c27caa3bfded5c342bf6
danielchk <hollowdaniel1@gmail.com>
2020-02-06 22:02:16 +0000

    Excercise -6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int z = 48;
+while (z <= 57)
+{
+putchar(z++);
+}
+putchar('\n');
+return (0);
+}
