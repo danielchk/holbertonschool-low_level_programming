d9c4bb70cfacb3cae4de81ac78efcd3893c0eeae
danielchk <hollowdaniel1@gmail.com>
2020-02-06 20:06:16 +0000

    fixing 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char i='abcdefghijklmnopqrstuwxyz';
-	putchar(i)\n;
-	return(0)
-		}
+char az[26] = "abcdefghijklmnopqrstuwxyz";
+int h = 0;
+char alfa;
+while (h < 26)
+{
+alfa = az[h];
+putchar(alfa);
+h++;
+}
+putchar ('\n');
+return(0);
+}
