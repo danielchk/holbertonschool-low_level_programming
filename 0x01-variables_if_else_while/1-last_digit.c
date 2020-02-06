6e91b291b1c9b3e08c762d1b532c2362a521e1d8
danielchk <hollowdaniel1@gmail.com>
2020-02-06 17:02:14 +0000

    Excercercises 1-3

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+int r = n%10;
+if ( r > 5 )
+{printf( "Last digit of %d is %d", n, r," and is greater than 5/n")};
+if ( r == 0)
+{printf( "Last digit of %d is %d", n, r,"and is 0\n")};
+if ( r < 6 && r > 0 )
+{printf( "Last digit of %d is %d", n, r,"and is less than 6 and not 0\n")};
+return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void)
+{
+	char i='abcdefghijklmnopqrstuwxyz';
+	putchar(i)\n;
+	return(0)
+		}
