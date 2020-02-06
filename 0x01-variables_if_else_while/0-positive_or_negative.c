4016fa8c585ef88b6fa200e3c142cf092293d4bc
danielchk <hollowdaniel1@gmail.com>
2020-02-06 17:10:43 +0000

    fixing 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,10 +11,10 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-{printf("%d", n"is positive")};
+{printf("%d", n,"is positive")};
 if (n == 0)
-{printf("%d", n"is zero")};
+{printf("%d", n,"is zero")};
 if (n < 0)
-{printf("%d", n"is negative")};
+{printf("%d", n,"is negative")};
 return (0);
 }
