bc6da1596757a278c0650a36f39f9ca1f33ed065
danielchk <hollowdaniel1@gmail.com>
2020-02-06 23:45:39 +0000

    Excercise 11

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for  (a = 48; a < 58; a++)
+	{
+		for (b = 48; b < 58; b++)
+		{
+			if (b > a)
+			{
+			putchar(a);
+			putchar(b);
+			}
+			if (b > a && a != 56)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+putchar('\n');
+return (0);
+}
