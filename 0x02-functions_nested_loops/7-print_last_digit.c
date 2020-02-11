5509a21ab9f3abfb41ee934d2110217044fb87cc
danielchk <hollowdaniel1@gmail.com>
2020-02-11 18:16:27 +0000

    Excercise 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_last_digit - Entry point
+ * @n: input
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int n)
+{
+	if (n < 0)
+	{
+		n = -n;
+	}
+	_putchar(n % 10 + '0');
+	return (n % 10);
+}
