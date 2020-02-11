a5a94968bd7c8eef8d58462e37ab0b18c9fb6e88
danielchk <hollowdaniel1@gmail.com>
2020-02-11 16:27:38 +0000

    Excercise 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_sign - Entry point
+ * @n: input
+ * Return: Always 0 (Success)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
