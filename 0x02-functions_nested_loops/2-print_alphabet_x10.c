fd9036d8814d23b02e06eaf2ce098b01a9d51359
danielchk <hollowdaniel1@gmail.com>
2020-02-11 15:14:29 +0000

    Excercise 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int a;
+	char c;
+
+	for (a = 0 ; a < 10 ; a++)
+	{
+		for (c = 'a' ; c <= 'z' ; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
