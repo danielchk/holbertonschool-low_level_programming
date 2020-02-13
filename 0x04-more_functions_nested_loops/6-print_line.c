29361d183c4aea42332dadd9690e6284791901c7
danielchk <hollowdaniel1@gmail.com>
2020-02-13 18:21:51 +0000

    fixing 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,10 +7,16 @@
 void print_line(int n)
 {
 	int a;
-
-	for (a = 0 ; a <= n ; a++)
+	if (n <= 0)
 	{
+		_putchar('\n');
+	}
+	else if
+	{
+		for (a = 0 ; a <= n ; a++)
+		{
 		_putchar(95);
+		}
 	}
 	_putchar('\n');
 }
