e7da11d3b969552768dd1d9661d1957c87fad017
danielchk <hollowdaniel1@gmail.com>
2020-02-20 23:13:36 +0000

    Excercise 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,4 +4,22 @@
  *
  * Return: Always 0.
  */
-int main(void)
+char *leet(char *u)
+{
+	int j, t;
+	int i[5] = {52, 51, 48, 55, 49};
+	char c[5] = {'a', 'e', 'o', 't', 'l'};
+	char h[5] = {'A', 'E', 'O', 'T', 'L'};
+
+	for (j = 0; u[j]; j++)
+	{
+		for (t = 0; t <= 4; t++)
+		{
+			if (u[j] == c[t] || u[j] == h[t])
+			{
+				u[j] = i[t];
+			}
+		}
+	}
+	return (u);
+}
