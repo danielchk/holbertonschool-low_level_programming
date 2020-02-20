2652591456bf3a1863999c13b355d3c8c8206443
danielchk <hollowdaniel1@gmail.com>
2020-02-20 18:53:55 +0000

    Fixing 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,22 +7,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
+	int i = 0;
 
-	for (i = 0 ; s1[i] != '\0'; i++)
-		;
-	for (j = 0 ; s2[j] != '\0'; j++)
-		;
-	if (i < j)
+	while (s1[i] == s2[i])
 	{
-		return (-15);
+		i++;
+		return (s1[i] - s2[1]);
 	}
-	else if (j < i)
-	{
-		return (15);
-	}
-	else
-	{
-		return (0);
-	}
+	return (s1[i] - s2[i]);
 }
