6a311391e4761daad9c752a37c71215e5932c211
danielchk <hollowdaniel1@gmail.com>
2020-02-20 21:04:33 +0000

    Fixing 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,4 @@
-A#include "holberton.h"
+#include "holberton.h"
 /**
  * *cap_string - check the code for Holberton School students.
  * @u: counter
@@ -41,7 +41,9 @@ for (i = 0; u[i]; i++)
 	if (u[i - 1] == '}')
 	{
 		if (u[i] > 96 && u[i] < 123)
+		{
 			u[i] = u[i] - 32;
+		}
 	}
 }
 return (u);
