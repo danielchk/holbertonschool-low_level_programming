9bfc0ee81971f92fe45997908c45a9e5ca8aa2f5
danielchk <hollowdaniel1@gmail.com>
2020-02-20 20:57:45 +0000

    Excercise 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+/**
+ * *cap_string - check the code for Holberton School students.
+ * @i: counter
+ * Return: Always 0.
+ */
+char *cap_string(char *u)
+{
+int i;
+
+for (i = 0; u[i]; i++)
+{
+	if (u[i - 1] == ' ' || u[i - 1] == 9 || u[i - 1] == '\n')
+	{
+		if (u[i] > 96 && u[i] < 123)
+		{
+			u[i] = u[i] - 32;
+		}
+	}
+	if (u[i - 1] == ',' || u[i - 1] == ';' || u[i - 1] == '.')
+	{
+		if (u[i] > 96 && u[i] < 123)
+		{
+			u[i] = u[i] - 32;
+		}
+	}
+	if (u[i - 1] == '!' || u[i - 1] == '?' || u[i - 1] == '\"')
+	{
+		if (u[i] > 96 && u[i] < 123)
+		{
+			u[i] = u[i] - 32;
+		}
+	}
+	if (u[i - 1] == '(' || u[i - 1] == ')' || u[i - 1] == '{')
+	{
+		if (u[i] > 96 && u[i] < 123)
+		{
+			u[i] = u[i] - 32;
+		}
+	}
+	if (u[i - 1] == '}')
+	{
+		if (u[i] > 96 && u[i] < 123)
+		{
+			u[i] = u[i] - 32;
+		}
+	}
+}
+return(u);
+}
