dad3f808e8dafbb7f00c5634ef5db88f83cfad2c
danielchk <hollowdaniel1@gmail.com>
2020-02-24 23:29:43 +0000

    Excercise 9

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * set_string - changes a value
+ * @s: doble pointer
+ * @to: a pointer
+ * Return: Always 0.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
