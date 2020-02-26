a5ba782ab6b4d80d61a31d77f95e72866074ee41
danielchk <hollowdaniel1@gmail.com>
2020-02-26 22:28:47 +0000

    Excercise 7

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void main(void)
+{
+	printf("%d \n", 1);
+	printf("%d \n", 1);
+	printf("%d \n", 0);
+	printf("%d \n", 1);
+}
