e7a53f55ecdd3d50941223f3a71487c3efcb2d39
danielchk <hollowdaniel1@gmail.com>
2020-03-01 16:28:42 +0000

    Excercise 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Argc, argv
+ * @argc: Arguments to run
+ * @argv: Array of strings
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc >=1)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,7 @@
+ARGC - ARGV
+
+0- Print the program name
+1- Number of arguments
+2- Print the arguments  it recives
+3- Multiplies two numbers
+4- Adds positive numbers
