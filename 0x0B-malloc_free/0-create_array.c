efbe58cfbb1cdb5364366f67fec4bfe05b3561c6
danielchk <hollowdaniel1@gmail.com>
2020-03-03 18:56:26 +0000

    Excercise 1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "holberton.h"
 /**
  * *create_array - array
  * @size: size of the arrays
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *_strdup
+ * @str: the string
+ * Return: Always 0.
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int i;
+	int size;
+
+	for (i = 0; str[i] != '\0'; i++)
+		size = i;
+
+	s = malloc(sizeof(char) * i);
+
+	for (i = 0; i <= size ; i++)
+		s[i] = str[i];
+
+	return (s);
+}
