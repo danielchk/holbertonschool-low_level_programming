36771891e9756a2b9218ebe01ece2d3c27ceb929
danielchk <hollowdaniel1@gmail.com>
2020-03-03 20:30:11 +0000

    Excercise 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * *str_concat - concatenate two strings
+ * @s1: string
+ * @s2: string 2
+ * Return: Always 0.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int size1, size2, sum, i, j;
+	char *con;
+
+	for (size1 = 0; s1[size1]; size1++)
+	{
+	}
+	for (size2 = 0; s2[size2]; size2++)
+	{
+	}
+	sum = size1 + size2 - 1;
+	con = malloc(sum * sizeof(char));
+	if (con == '\0')
+	{
+		return ('\0');
+	}
+	for (i = 0; i < size1; i++)
+	{
+		con[i] = s1[i];
+	}
+	for (j = 0; j < size2; j++)
+	{
+		con[size1 + j] = s2[j];
+	}
+	return (con);
+}
