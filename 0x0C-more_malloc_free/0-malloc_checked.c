a3f9f56ebcb7a5cfe4288cc5106bab2c38517195
danielchk <hollowdaniel1@gmail.com>
2020-03-06 02:26:22 +0000

    Excercise 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * *malloc_checked - do the malloc to b
+ * @b: Int
+ * Return: Always 0.
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *s;
+
+	s = malloc(b);
+	if (s == '\0')
+	{
+		exit(98);
+	}
+	return(s);
+}
