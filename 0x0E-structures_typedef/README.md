c385a925e56faf67b74f09e9234669ac8ae01406
danielchk <hollowdaniel1@gmail.com>
2020-03-09 19:58:14 +0000

    Excercise 0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,8 @@
+STRUCTURES
+
+0- New type structure
+1- Initialize the new variable
+2- Print a new variable
+3- Define a new varibale type for another new
+4- Create a new dog
+5- Free the dogs
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - New type
+ * @name: the name
+ * @age: The age
+ * @owner: The owner
+ * Description: The dog with data
+ */
+
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+ #endif
