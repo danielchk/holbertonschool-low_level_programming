aee26dd75b651f44061b326b618ebe3d119f0eb3
danielchk <hollowdaniel1@gmail.com>
2020-03-09 17:05:47 +0000

    excercise 0 to 4

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define NAME __FILE__
+
+/**
+ * main - Print the name
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", NAME);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,7 @@
+PREPROCESSOR
+
+0- Define a macro SIZE
+1- Define a macro of PI
+2- Macro that prints the name of the file
+3- Computes the absolute valor of a nuber
+4- Add X and Y
