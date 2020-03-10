c4ef300d8ebd682d04f84e92240224b301492020
danielchk <hollowdaniel1@gmail.com>
2020-03-10 00:00:42 +0000

    Fixing 5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,16 +1,15 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- * 
- * 
+ * free_dog - free
+ * @d: Data of the dogs
  */
 void free_dog(dog_t *d)
 {
-    if (d != NULL)
-    {
-    free(d->name);
-    free(d->owner);
-    free(d);
-    }
-
+	if (d != NULL)
+	{
+	free(d->name);
+	free(d->owner);
+	free(d);
+	}
 }
