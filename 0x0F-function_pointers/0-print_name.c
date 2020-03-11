843a4f8a41d80808c4473408c03f259cae960a4f
danielchk <hollowdaniel1@gmail.com>
2020-03-11 17:45:08 +0000

    fixing 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stddef.h>
 /**
  * print_name - Prints the name
  * @name: name of the person
@@ -10,7 +9,5 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == '\0')
-		return ('\0');
 	f(name);
 }
