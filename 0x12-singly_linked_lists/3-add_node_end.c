e09d77ea037cb4c81c15a8ba622ae0a6f57a0ee3
danielchk <hollowdaniel1@gmail.com>
2020-03-20 03:18:07 +0000

    Fixing betty in 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -8,31 +8,31 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *temp, *node;
-    int i = 0;
+	list_t *temp, *node;
+	int i = 0;
 
-    node = malloc(sizeof(list_t));
-    if (node == NULL)
-        return (NULL);
+	node = malloc(sizeof(list_t));
+	if (node == NULL)
+		return (NULL);
 
-    if (*head != NULL)
-    {
-        temp = *head;
-        while (temp->next != NULL)
-            temp = temp->next;
-        
-        temp->next = node;
-    }
-    else
-    {
-    *head = node;
-    }
-    node->next = NULL;
-    node->str = strdup(str);
+	if (*head != NULL)
+	{
+		temp = *head;
+		while (temp->next != NULL)
+			temp = temp->next;
 
-    while(str[i] != '\0')
-        i++;
-    
-    node->len = i;
-    return (node);
+		temp->next = node;
+	}
+	else
+	{
+	*head = node;
+	}
+	node->next = NULL;
+	node->str = strdup(str);
+
+	while (str[i] != '\0')
+		i++;
+
+	node->len = i;
+	return (node);
 }
