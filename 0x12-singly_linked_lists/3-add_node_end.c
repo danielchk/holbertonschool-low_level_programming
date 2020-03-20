146002e6ebad979f90902dfb7dc2df62a796d056
danielchk <hollowdaniel1@gmail.com>
2020-03-20 02:12:32 +0000

    Excercise 3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * *add_node_end - Add node to end
+ * @head: head
+ * @str: new string
+ * Return: node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+    list_t *temp, *node;
+    int i = 0;
+
+    node = malloc(sizeof(list_t));
+    if (node == NULL)
+        return (NULL);
+
+    if (*head == NULL)
+    {
+        temp = *head;
+        while (temp->next != NULL)
+            temp = temp->next;
+        
+        temp->next = node;
+    }
+    else
+    {
+    *head = node;
+    }
+    node->next = NULL;
+    node->str = strdup(str);
+
+    while(str[i] != '\0')
+        i++;
+    
+    node->len = i;
+    return (node);
+}
