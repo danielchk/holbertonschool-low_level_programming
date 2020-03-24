65011b6cf82fb5917aaec2aaf6271c4b4126c7de
danielchk <hollowdaniel1@gmail.com>
2020-03-24 15:44:10 +0000

    Excercise 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * pop_listint - delete head and show his data
+ *
+ * @head: head
+ * Return: old head
+ */
+int pop_listint(listint_t **head)
+{
+	int ubicationold;
+	listint_t *new;
+
+	if (head == NULL)
+		return (0);
+
+	ubicationold = (*head)->n;
+	new = (*head)->next;
+	free(*head);
+	*head = new;
+
+	return (ubicationold);
+}
