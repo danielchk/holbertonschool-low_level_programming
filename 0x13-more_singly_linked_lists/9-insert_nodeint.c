a8a03bfa3c410756f8b7b780e51beaf48f3a7ace
danielchk <hollowdaniel1@gmail.com>
2020-03-24 20:58:24 +0000

    fixing too much 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	 unsigned int i = 0;
 	 listint_t *node, *nextnode;
 
-	if (head == NULL || *head == NULL)
+	if ((head == NULL || *head == NULL) && idx != 0)
 		return (NULL);
 
 	nextnode = *head;
