403c17bb771f7c618a2d4d72ed15a6abe1fe2c8d
danielchk <hollowdaniel1@gmail.com>
2020-03-26 22:18:05 +0000

    Excercise 1

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * binary_to_uint - Convert to unsigned int.
+ * @b: To convert to decimal
+ * Return: unsigned int.
+ */
+ unsigned int binary_to_uint(const char *b)
+ {
+     unsigned int value = 0;
+     int i;
+     if (b == NULL)
+        return (0);
+
+    for (i = 0; b[i] != '\0'; i++)
+    {
+        if (b[i] != '1' && b[i] != '0')
+            return (0);
+
+        value = (value * 2) + (b[i] - '0');
+    }
+    return (value);
+ }
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,8 @@
+BIT MANIPULATION
+
+0- Convert a binary to unsigned int
+1- Prints the binary representation of a number
+2- Returns the value of a bit at a given index
+3- Sets the value of a bit to 1 at a given index
+4- Sets the value of a bit to 0 at a given index
+5- Returns the number of bits you would need to flip to get from one number to another
