85417be66ecf0cda4eb6e47ce0a9b141de4bd329
danielchk <hollowdaniel1@gmail.com>
2020-03-26 22:21:56 +0000

    Fixing 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,19 +4,20 @@
  * @b: To convert to decimal
  * Return: unsigned int.
  */
- unsigned int binary_to_uint(const char *b)
- {
-     unsigned int value = 0;
-     int i;
-     if (b == NULL)
-        return (0);
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int value = 0;
+	int i;
 
-    for (i = 0; b[i] != '\0'; i++)
-    {
-        if (b[i] != '1' && b[i] != '0')
-            return (0);
+	if (b == NULL)
+		return (0);
 
-        value = (value * 2) + (b[i] - '0');
-    }
-    return (value);
- }
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '1' && b[i] != '0')
+			return (0);
+
+		value = (value * 2) + (b[i] - '0');
+	}
+	return (value);
+}
