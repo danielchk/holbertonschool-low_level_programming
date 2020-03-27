a12bde484cf477f810e8ffc15af4f92d55fbb829
danielchk <hollowdaniel1@gmail.com>
2020-03-27 23:03:52 +0000

    fixing 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,10 +1,31 @@
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * print_binary - number in binary
+ * @n: number to binary
+ * Return: Nothing
  */
- void print_binary(unsigned long int n)
- {
-    fuck!
- }
+void print_binary(unsigned long int n)
+{
+	unsigned long int bin;
+	int i, j;
+
+	if (n == '0')
+		_putchar('0');
+
+	bin = n
+	for (i = 0; bin; i++)
+		bin = bin >> 1;
+
+	for (i - 1; i; i--)
+	{
+		j = n >> i;
+		if (j & 1)
+		{
+			_putchar('1');
+		}
+		else
+		{
+			_putchar('0');
+		}
+	}
+}
