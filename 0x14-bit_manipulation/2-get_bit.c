d67fdac4364d48050a240feac574270562022d87
danielchk <hollowdaniel1@gmail.com>
2020-03-27 01:25:14 +0000

    fixing 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,10 +1,11 @@
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * get_bit - show binary
+ * @index: position to check
+ * @n: number
+ * Return: binary if it works and -1 if error.
  */
- int get_bit(unsigned long int n, unsigned int index)
+int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int bin;
 
