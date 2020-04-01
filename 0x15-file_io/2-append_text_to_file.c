4bddc8c8970c8675c0b32d0be84f365458491ef3
danielchk <hollowdaniel1@gmail.com>
2020-04-01 00:25:10 +0000

    Fixing 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,7 +17,10 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
+	{
+		close(name);
 		return (-1);
+	}
 
 	sizer = 0;
 	while (text_content[sizer])
