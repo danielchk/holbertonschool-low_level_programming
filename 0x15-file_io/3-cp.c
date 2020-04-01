77723ed962c229e404f634f14a1ac0ffde6786d7
danielchk <hollowdaniel1@gmail.com>
2020-04-01 12:42:34 +0000

    Fixing 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,83 +1,35 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-/**
- * main - copies content to other file
- * @argc: number of arguments
- * @argv: arguments to add
- * Return: 1 or -1 - Failure.
- */
+#include "holberton.h"
+
 int main(int argc, char *argv[])
 {
-	int source, destiny, copy;
+	int from, dest, reader = 1024, count = 0;
+	char buffer[BUFFER_SIZE];
 
-	if (argc != 3 || argv == NULL)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	source = open(argv[1], O_RDONLY);
-	if (source == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't reader source file %s\n", argv[1]);
-		exit(98);
-	}
-	destiny = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (destiny == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	copy = copy_to(source, destiny, argv[1], argv[2]);
-	return (copy);
-}
-/**
- * copy_to - function that copies the content of a file to another file.
- * @source: source
- * @destiny: destination
- * @namesource: name file source
- * @namedest: name to destiny
- * Return: 1 - Success or -1 - Failure.
- */
-int copy_to(int source, int destiny, char *namesource, char *namedest)
-{
-	int reader, writer, close1, close2;
-	char buff[1024];
+	if (argc != 3)
+		dprintf(STDERR_FILENO, USAGE), exit(97);
 
-	do {
-		reader = reader(source, buff, 1024);
+	from = open(argv[1], O_RDONLY);
+	if (from == -1)
+		dprintf(STDERR_FILENO, FILE_READ_ERROR, argv[1]), exit(98);
 
+	dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (dest == -1)
+		dprintf(STDERR_FILENO, FILE_WRITE_ERROR, argv[2]), exit(99);
+
+	while (reader == BUFFER_SIZE)
+	{
+		reader = read(from, buffer, BUFFER_SIZE);
 		if (reader == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't reader source file %s\n", namesource);
-			exit(98);
-		}
+			dprintf(STDERR_FILENO, FILE_READ_ERROR, argv[1]), exit(98);
 
-		writer = write(destiny, buff, reader);
-
-		if (writer == -1 || reader != writer)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", namedest);
-			exit(99);
-		}
-
-	} while (reader == 1024);
-
-	close1 = close(source);
-
-	if (close1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", source);
-		exit(100);
+		count = write(dest, buffer, reader);
+		if (count == -1)
+			dprintf(STDERR_FILENO, FILE_WRITE_ERROR, argv[2]), exit(99);
 	}
-	close2 = close(destiny);
-	if (close2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", destiny);
-		exit(100);
-	}
+	if (close(from) == -1)
+		dprintf(STDERR_FILENO, FILE_CLOSE_ERROR, from), exit(100);
+
+	if (close(dest) == -1)
+		dprintf(STDERR_FILENO, FILE_CLOSE_ERROR, dest), exit(100);
 	return (0);
 }
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -6,6 +6,12 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+#define USAGE "Usage: cp file_from file_to\n"
+#define FILE_READ_ERROR "Error: Can't read from file %s\n"
+#define FILE_WRITE_ERROR "Error: Can't write to %s\n"
+#define FILE_CLOSE_ERROR "Error: Can't close fd  %d\n"
+#define BUFFER_SIZE 1024
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
