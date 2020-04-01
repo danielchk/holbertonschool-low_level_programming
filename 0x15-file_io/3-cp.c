ab3a72d43b6e7b44c6316d91804132b8c4dc9c9a
danielchk <hollowdaniel1@gmail.com>
2020-04-01 03:47:03 +0000

    Fixing 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,75 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <sys/types.h>
-#include <unistd.h>
 /**
- * main - copies the content of a file to another file
- * @argc: count arguments
- * @argv: array of arguments
- * Return: 0
+ * main - copies content to other file
+ * @argc: number of arguments
+ * @argv: arguments to add
+ * Return: 1 or -1 - Failure.
  */
 int main(int argc, char *argv[])
 {
-	int from, destiny, i, k;
-	char buff[1024];
-	ssize_t r, w;
+	int source, destiny, copy;
 
-	if (argc != 3)
+	if (argc != 3 || argv == NULL)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp from destiny\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	for (i = 0; i < 1024; i++)
-		buff[i] = 0;
-	from = open(argv[1], O_RDONLY);
-	if (from == -1)
+	source = open(argv[1], O_RDONLY);
+	if (source == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
-			argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't reader source file %s\n", argv[1]);
 		exit(98);
 	}
-	destiny = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	destiny = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (destiny == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
-			argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	r = read(from, buff, 1024);
-	if (r == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
-			argv[1]);
-		exit(98);
-	}
-	while (r)
-	{
-		w = write(destiny, buff, r);
-		if (w == -1)
+	copy = copy_to(source, destiny, argv[1], argv[2]);
+	return (copy);
+}
+/**
+ * copy_to - function that copies the content of a file to another file.
+ * @source: source
+ * @destiny: destination
+ * @namesource: name file source
+ * @namedest: name to destiny
+ * Return: 1 - Success or -1 - Failure.
+ */
+int copy_to(int source, int destiny, char *namesource, char *namedest)
+{
+	int reader, writer, close1, close2;
+	char buff[1024];
+
+	do {
+		reader = reader(source, buff, 1024);
+
+		if (reader == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
-				argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't reader source file %s\n", namesource);
+			exit(98);
+		}
+
+		writer = write(destiny, buff, reader);
+
+		if (writer == -1 || reader != writer)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", namedest);
 			exit(99);
 		}
 
-		r = read(from, buff, 1024);
-		if (r == -1)
-		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
-	}
-	k = close(from);
-	if (k == -1)
+	} while (reader == 1024);
+
+	close1 = close(source);
+
+	if (close1 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", source);
 		exit(100);
 	}
-	k = close(destiny);
-	if (k == -1)
+	close2 = close(destiny);
+	if (close2 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", destiny);
 		exit(100);
