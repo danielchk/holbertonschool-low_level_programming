83e5388aafb0014d3ae013352913b77e905b0ef3
danielchk <1487@holbertonschool.com>
2020-05-04 01:11:56 +0000

    betty error

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,12 +16,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx != 0)
 	{
 		actual = *h;
-
 while (i < idx - 1 && actual != NULL)
 {
-			    actual = actual->next;
-                i++;
-            }
+actual = actual->next;
+i++;
+}
 		if (actual == NULL)
 			return (NULL);
 	}
