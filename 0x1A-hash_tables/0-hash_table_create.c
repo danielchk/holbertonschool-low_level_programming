6ca7984e138f97607a63d5b9db2d62829ca77bd7
danielchk <1487@holbertonschool.com>
2020-05-29 14:10:26 +0000

    Excercises 0,1,2

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - create hash table
+ * @size: size of the hash table
+ * Return: hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hasht;
+	unsigned long int i;
+
+	hasht = malloc(sizeof(hash_table_t));
+	if (hasht == NULL)
+		return (NULL);
+
+	hasht->size = size;
+	hasht->array = malloc(sizeof(hash_table_t) * size);
+	if (hasht->array == NULL)
+	{
+		free(hasht);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+		hasht->array[i] = NULL;
+	return (hasht);
+}
