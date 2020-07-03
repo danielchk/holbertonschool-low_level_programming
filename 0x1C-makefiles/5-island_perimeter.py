273abe51fac03c995e177410b40a15a23718c29d
danielchk <hollowdaniel1@gmail.com>
2020-07-03 17:18:02 +0000

    Excercises

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -15,8 +15,8 @@ def island_perimeter(grid):
         for j in range(w):
             if grid[i][j] == 1:
                 total += 1
-                if (j >= 0 and grid[i][j - 1] == 1):
+                if (j > 0 and grid[i][j - 1] == 1):
                     less += 1
-                if (i >= 0 and grid[i - 1][j] == 1):
+                if (i > 0 and grid[i - 1][j] == 1):
                     less += 1
     return total * 4 - less * 2
